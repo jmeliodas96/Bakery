6e7dcb69600aaeb97dab9c4061ee2d49f2fd0c12
Billy Palacio <billy.palacios@hotmail.com>
2020-03-02 17:29:58 -0600

    add gmail sender email and gitignore

--- Pages/Order.cshtml.cs
+++ Pages/Order.cshtml.cs
@@ -1,7 +1,8 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks;
+using System.Net;
 using System.Net.Mail;
+using System.Threading.Tasks;
 using Bakery.Data;
 using Bakery.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -13,44 +14,57 @@ namespace Bakery.Pages
         //inject Bakery Context
         private BakeryContext db;
         public OrderModel(BakeryContext db) => this.db = db;
-        [BindProperty(SupportsGet =true)]
+        [BindProperty(SupportsGet = true)]
         public int Id { get; set; }
-        public Product Product { get; set;}
-        [BindProperty, EmailAddress, Required, Display(Name="Your Email Address")]
+        public Product Product { get; set; }
+
+        [BindProperty, EmailAddress, Required, Display(Name = "Your Email Address")]
         public string OrderEmail { get; set; }
-        [BindProperty, Required(ErrorMessage="Please supply a shipping address"), Display(Name="Shipping Address")]
-        public string OrderShipping { get; set; } 
-        [BindProperty, Display(Name="Quantity")]
+
+        [BindProperty, Required(ErrorMessage = "Please supply a shipping address"), Display(Name = "Shipping Address")]
+        public string OrderShipping { get; set; }
+
+        [BindProperty, Display(Name = "Quantity")]
         public int OrderQuantity { get; set; } = 1;
-        public async Task OnGetAsync() =>  Product = await db.Products.FindAsync(Id);
+        public async Task OnGetAsync() => Product = await db.Products.FindAsync(Id);
 
         public async Task<IActionResult> OnPostAsync()
         {
             Product = await db.Products.FindAsync(Id);
-            if(ModelState.IsValid){
-                
+            if (ModelState.IsValid)
+            {
+
                 var body = $@"<p>Thank you, we have received your order for {OrderQuantity} unit(s) of {Product.Name}!</p>
                 <p>Your address is: <br/>{OrderShipping.Replace("\n", "<br/>")}</p>
                 Your total is ${Product.Price * OrderQuantity}.<br/>
                 We will contact you if we have questions about your order.  Thanks!<br/>";
 
-                using(var smtp = new SmtpClient())
+                var smtp = new SmtpClient()
                 {
-                    smtp.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
-                    smtp.PickupDirectoryLocation =  @"C:\Users\Jimmy\Documents\emails";
-                    var message = new MailMessage();
-                    message.To.Add(OrderEmail);
-                    message.Subject = "Fourth Coffee - New Order";
-                    message.Body = body;
-                    message.IsBodyHtml = true;
-                    message.From = new MailAddress("sales@fourthcoffee.com");
-                    await smtp.SendMailAsync(message);
-                }
-                
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("billypalacio25@gmail.com", "masquinita#174"),
+                    Port = 587,
+                    Host = "smtp.gmail.com",
+                    EnableSsl = true,
+                };
+
+                var message = new MailMessage();
+                message.To.Add(OrderEmail);
+                message.Subject = "Fourth Coffee - New Order";
+                message.Body = body;
+                message.IsBodyHtml = true;
+                message.From = new MailAddress("sales@fourthcoffee.com");
+                await smtp.SendMailAsync(message);
+
+
+
+
                 return RedirectToPage("OrderSuccess");
-                }   
+            }
             return Page();
         }
-
-    }    
+    }
 }
+
+
